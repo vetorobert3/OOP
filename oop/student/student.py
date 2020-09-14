540ba24bdf873da6980d1ddd52ffb5a559fe37a3
Veto Ramirez <vetorobert3@gmail.com>
2020-09-13 18:05:43 -0700

    Composition practice

--- oop/student/student.py
+++ oop/student/student.py
@@ -1,21 +1,12 @@
 class Student:
-    def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
+    def __init__(self, name):
+        self.name = name
+        
+class Graduate(Student):
+    def __init__(self, name, graduation_date):
+        super().__init__(name)
+        self.graduation_date =  graduation_date
 
-# student = Student()
-# print(student)
-# print(student.first_name, student.last_name)
-
-class Time:
-    def __init__(self, hour, minute, second):
-        self.hour = hour
-        self.minute = minute
-        self.second = second
-
-    def __str__(self):
-        return '%.2d:%.2d:%.2d' % (self.hour, self.minute, self.second)
-
-
-time = Time(3, 17, 56)
-print(time)
+john_lennon = Graduate('John Lennon', 'December 8, 1980')
+print(john_lennon.name)
+print(john_lennon.graduation_date)
